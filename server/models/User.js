3472478448ae9cc65c6a29a7f32a49aec8a3ec07
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-02-04 21:15:02 +0800

    Setup User schema

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,28 @@
 const { model, Schema } = require('mongoose');
 
 const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First name is required']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Last name is required']
+    },
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
+    },
     isActive: {
         type: Boolean,
         default: true
