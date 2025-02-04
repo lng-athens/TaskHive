07e92bb144111ad4f031593841f2313ecb14e6d0
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-02-04 22:08:52 +0800

    Finished CreateUser function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,39 @@
 const expressAsyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const { connectDB } = require('../configs/database.conf');
 
+const CreateUser = expressAsyncHandler(async (req, res) => {
+    const { firstName, lastName, username, email, password } = req.body;
 
+    const db = await connectDB();
+    const userCollection = db.collection('users');
+    const filter = {$or: [{username: username}, {email: email}]};
 
-module.exports = {};
+    const user = await userCollection.findOne(filter);
+    if (user) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+        firstName,
+        lastName,
+        username,
+        email,
+        password: hashedPassword
+    });
+
+    const addUserResult = await userCollection.insertOne(newUser);
+    if (!addUserResult.acknowledged || !addUserResult.insertedId) {
+        res.status(500);
+        throw new Error('Failed to create user');
+    }
+
+    res.status(201).send({message: 'User created successfully'});
+});
+
+module.exports = {
+    CreateUser
+};
