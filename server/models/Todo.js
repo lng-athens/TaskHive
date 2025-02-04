03c8747dfe943d6393f2d626aa44c5d80b99d74c
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-02-04 21:15:10 +0800

    Setup Todo schema

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,6 +1,26 @@
 const { model, Schema } = require('mongoose');
 
 const todoSchema = new Schema({
+    task: {
+        type: String,
+        required: [true, 'Task to do is required']
+    },
+    category: {
+        type: String,
+        enum: [
+            'Immediate',
+            'Urgent',
+            'High Priority',
+            'Moderate Priority',
+            'Low Priority'
+        ],
+        required: true,
+        message: 'Category must be one of: Immediate, Urgent, High Priority, Moderate Priority, Low Priority'
+    },
+    deadline: {
+        type: Date,
+        required: [true, 'Deadline of task is required']
+    },
     isActive: {
         type: Boolean,
         default: true
