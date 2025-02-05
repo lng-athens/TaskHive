b92f2a67c31bcffc7c38cf074c967b81d1951985
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-02-05 14:38:21 +0800

    Finished LoginUser function

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,11 @@
 const expressAsyncHandler = require('express-async-handler');
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const UserKey = require('../models/UserKey');
 const { connectDB } = require('../configs/database.conf');
 const logger = require('../utils/Logger');
+const { v7: uuidv7 } = require('uuid');
+const { generateToken } = require('../services/authService');
 
 const CreateUser = expressAsyncHandler(async (req, res) => {
     const { firstName, lastName, username, email, password } = req.body;
@@ -38,6 +41,86 @@ const CreateUser = expressAsyncHandler(async (req, res) => {
     res.status(201).send({message: 'User created successfully'});
 });
 
+const LoginUser = expressAsyncHandler(async (req, res) => {
+    const { userId, password } = req.body;
+    const userIp = req.ip;
+    const userAgent = req.useragent;
+
+    const db = await connectDB();
+    const userCollection = db.collection('users');
+    const userkeyCollection = db.collection('user_keys');
+    const filter = {$or: [{username: userId}, {email: userId}]};
+
+    const user = await userCollection.findOne(filter);
+    if (!user) {
+        logger.error('User not found');
+        res.status(404);
+        throw new Error('User not found');
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+        logger.error('Incorrect password');
+        res.status(400);
+        throw new Error('Incorrect password');
+    }
+
+    const payload = {
+        iss: "TaskHive",
+        sub: user._id.toString(),
+        aud: ["http://localhost:5173"],
+        iat: Math.floor(Date.now() / 1000),
+        jti: uuidv7(),
+        user: {
+            firstName: user.firstName,
+            lastName: user.lastName,
+            username: user.username,
+            email: user.email
+        },
+        ip: userIp,
+        userAgent: userAgent.source || 'unknown'
+    };
+
+    const { privateKey, publicKey, token } = await generateToken(payload);
+    logger.info('Token generated for: ', user.username);
+
+    const userkeyFilter = {$or: [{username: userId}, {email: userId}]};
+    const userkey = await userkeyCollection.findOne(userkeyFilter);
+    if (userkey) {
+        const updateResult = await userkeyCollection.updateOne(userkeyFilter, {$push: { privateKey }});
+
+        if (!updateResult.acknowledged) {
+            logger.error('Failed to add user key for: ', user.username);
+            res.status(500);
+            throw new Error('Failed to add user key');
+        }
+    }
+    else {
+        const newKey = new UserKey({
+            username: user.username,
+            email: user.email,
+            privateKey
+        });
+    
+        const addKeyResult = await userkeyCollection.insertOne(newKey);
+        if (!addKeyResult.acknowledged || !addKeyResult.insertedId) {
+            logger.error('Failed to add user key for: ', user.username);
+            res.status(500);
+            throw new Error('Failed to add user key');
+        }
+    }
+
+    res.cookie('__Secure-token', token , {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'Lax'
+    });
+    
+    logger.info(`User ${user.username} has been logged in`);
+    res.status(200).send({publicKey: publicKey});
+});
+
 module.exports = {
-    CreateUser
+    CreateUser,
+    LoginUser
 };
