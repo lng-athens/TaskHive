546841e6afebc8dd0aab23c70751f35c12fa014f
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-02-08 12:05:11 +0800

    Finished AddTask function

--- server/controllers/todoController.js
+++ server/controllers/todoController.js
@@ -1,7 +1,27 @@
 const expressAsyncHandler = require('express-async-handler');
 const Todo = require('../models/Todo');
 const { connectDB } = require('../configs/database.conf');
+const logger = require('../utils/Logger');
 
+const AddTask = expressAsyncHandler(async (req, res) => {
+    const { taskDetails, payload } = req.body;
+    
+    const db = await connectDB();
+    const todoCollection = db.collection('todos');
+    
+    taskDetails.owner_id = payload.sub;
 
+    const newTodo = new Todo(taskDetails);
+    const addTodoResult = await todoCollection.insertOne(newTodo);
+    if (!addTodoResult.acknowledged || !addTodoResult.insertedId) {
+        logger.error('Failed to add task');
+        res.status(500);
+        throw new Error('Failed to add task');
+    }
 
-module.exports = {};
+    res.status(201).send({message: 'Task added successfully'});
+});
+
+module.exports = {
+    AddTask
+};
