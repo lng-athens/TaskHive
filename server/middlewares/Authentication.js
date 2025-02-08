dd7f36baed9245807bc44e3a4e414eb20816c61e
lng-athens <gonzalez.lourdnathaniel@gmail.com>
2025-02-08 11:28:07 +0800

    Finished ValidateToken function

--- server/middlewares/Authentication.js
+++ server/middlewares/Authentication.js
@@ -1,6 +1,29 @@
 const expressAsyncHandler = require('express-async-handler');
 const auth = require('../services/authService');
+const logger = require('../utils/Logger');
 
+const ValidateToken = expressAsyncHandler(async (req, res, next) => {
+    const token = req.cookies['__Secure-token'];
+    if (!token) {
+        logger.error('No token provided');
+        res.status(404);
+        throw new Error('No token provided');
+    }
 
+    const { publicKeyJwk } = req.body;
+    if (!publicKeyJwk) {
+        logger.error('Public key not provided');
+        res.status(404);
+        throw new Error('Public key not provided');
+    }
 
-module.exports = {};
+    const payload = await auth.verifyToken(token, publicKeyJwk);
+    
+    req.body.user = payload.user;
+
+    next();
+});
+
+module.exports = {
+    ValidateToken
+};
